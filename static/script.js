efb93efbbccccfda5022e5c80f510e3c2125e2f2
Shriteen <86562935+Shriteen@users.noreply.github.com>
2023-06-11 12:09:52 +0530

    Background color change based on classification

--- static/script.js
+++ static/script.js
@@ -14,28 +14,35 @@ form.addEventListener('submit',function(e){
 	    {
 		case "not_cyberbullying":
 		resField.textContent= "Not Cyberbullying 😃";
+		setBGColor('#69f7ff');
 		break;
 
 		case "other_cyberbullying":
 		resField.textContent= "Other Cyberbullying";
+		setBGColor('#72b0f8');
 		break;
 
 		case "gender":
 		resField.textContent= "Bullying: Gender";
+		setBGColor('#ff22b3');
 		break;
 
 		case "ethnicity":
 		resField.textContent= "Bullying: Ethnicity";
+		setBGColor('#b145c5');
 		break;
 
 		case "religion":
 		resField.textContent= "Bullying: Religion";
+		setBGColor('#d6ff86');		
 		break;
 
 		case "age":
 		resField.textContent= "Bullying: Age";
+		setBGColor('#ee72f8');		
 		break;
 	    }
+	    setTimeout(()=>{setBGColor('#3a3a3a')},3000);
 	})
 	.catch((err) => {
 	    console.log(err);
@@ -45,3 +52,8 @@ form.addEventListener('submit',function(e){
     
     e.preventDefault();
 });
+
+function setBGColor(color)
+{
+    document.querySelector('body').style.backgroundColor=color;
+}
